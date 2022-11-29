653282a406d91337fc871ce632827ae5aed8dd59
praveenruhil <praveenruhil1993@gmail.com>
2022-11-29 16:51:38 -0500

    Add files via upload

---
+++ telnet.py
@@ -0,0 +1,10 @@
+import sys 
+import telnetlib
+
+HOST="python.org"
+PORT = "80"
+tn = telnetlib.Telnet(HOST,PORT)
+msg = ("GET /index.html HTTP/1.1\nHost:"+HOST+"\n\n").encode('ascii')
+tn.write(msg)
+print(tn.read_all())
+tn.close()
